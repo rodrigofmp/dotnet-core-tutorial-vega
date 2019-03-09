99a38c80e74e55da8d9bbdffc5bfe36896af3381
Rodrigo Petters <rodrigofmp@gmail.com>
2019-03-09 18:00:52 -0300

    Binding features section

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -10,7 +10,9 @@ export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
   features: any[];
-  vehicle: any = {};  
+  vehicle: any = {
+    features: []
+  };  
 
   constructor(private vehicleService: VehicleService) {
     this.makes = [];
@@ -27,6 +29,16 @@ export class VehicleFormComponent implements OnInit {
     var selectedMake = this.makes.find(m => m.id == this.vehicle.makeId);
     this.models = selectedMake ? selectedMake.models : [];
     delete this.vehicle.modelId;
+  }
+
+  onFeatureToggle(featureId: any, $event: any) {
+    if ($event.target.checked)
+      this.vehicle.features.push(featureId);
+    else {
+      var index = this.vehicle.features.indexOf(featureId);
+      this.vehicle.features.splice(index, 1);
+    }
+
   }
 
 }
